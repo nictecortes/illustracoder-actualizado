2020653316f80528c29165043430bda158a8ce79
Jorge Canche <jcanchecauich@gmail.com>
2024-07-22 19:35:15 -0600

    fix(js y css): Se agrega apuntado correcto al id y se agrega estilo al boton

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-window.addEventListener('scroll', reveal);
+window.addEventListener("scroll", reveal);
 
-function reveal(){
-    let reveals = document.querySelectorAll('.reveal');
+function reveal() {
+	let reveals = document.querySelectorAll(".reveal");
 
-    for(let i = 0; i < reveals.length; i++){
+	for (let i = 0; i < reveals.length; i++) {
+		let windowHeight = window.innerHeight;
+		let revealTop = reveals[i].getBoundingClientRect().top;
+		let revealPoint = 200;
 
-        let windowHeight = window.innerHeight;
-        let revealTop = reveals[i].getBoundingClientRect().top;
-        let revealPoint = 200;
-
-        if(revealTop < windowHeight - revealPoint){
-            reveals[i].classList.add('active');
-        }
-        else {
-            reveals[i].classList.remove('active');
-        }
-
-    }
-
+		if (revealTop < windowHeight - revealPoint) {
+			reveals[i].classList.add("active");
+		} else {
+			reveals[i].classList.remove("active");
+		}
+	}
 }
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-            event.preventDefault(); // Evitar que el formulario se envíe de manera tradicional
+document
+	.getElementById("contactForm")
+	.addEventListener("submit", function (event) {
+		event.preventDefault(); // Evitar que el formulario se envíe de manera tradicional
 
-            var formData = new FormData(this);
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', 'contact.php', true);
-            xhr.onload = function() {
-                if (xhr.status === 200) {
-                    var response = JSON.parse(xhr.responseText);
-                    var formResponse = document.getElementById('formResponse');
-                    formResponse.innerHTML = response.message;
-                    if (response.status === "success") {
-                        formResponse.style.color = "green";
-                    } else {
-                        formResponse.style.color = "red";
-                    }
-                } else {
-                    alert('An error occurred!');
-                }
-            };
-            xhr.send(formData);
-        });
+		var formData = new FormData(this);
+		var xhr = new XMLHttpRequest();
+
+		xhr.open("POST", "contact.php", true);
+		xhr.onload = function () {
+			console.log(xhr);
+			if (xhr.status === 200) {
+				var response = JSON.parse(xhr.responseText);
+				var formResponse = document.getElementById("form-message");
+				//formResponse.innerHTML = response.message;
+				if (response.status === "success") {
+					formResponse.style.color = "green";
+				} else {
+					formResponse.style.color = "red";
+				}
+			} else {
+				alert("An error occurred!");
+			}
+		};
+		xhr.send(formData);
+	});
